fc9cc49063e64c4a775b513f348b5e464182aacc
TeeGreat <toyinatofarati64@gmail.com>
2025-07-09 14:27:44 +0100

    tab navigation

---
+++ app/(tabs)/rufus.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Rufus = () => {
+  return (
+    <View>
+      <Text>Rufus</Text>
+    </View>
+  )
+}
+
+export default Rufus
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,27 @@
 import { Stack } from "expo-router";
+import {ThemeProvider, DarkTheme, DefaultTheme} from "@react-navigation/native";
+import { useColorScheme } from "react-native";
 import "./global.css";
+import { StyledStack } from "@/components/navigation/stack";
 
-export default function RootLayout() {
-  return <Stack />;
+const InitialLayout = () => {
+  return (
+    <StyledStack contentClassName="bg-gray-100 dark:bg-dark" headerClassName="bg-dark text-white">
+        <Stack.Screen name="(tabs)" options={{headerShown: false}} />
+    </StyledStack>
+
+  )
 }
+
+const  RootLayout =() => {
+  const colorScheme = useColorScheme() 
+  const theme = colorScheme === "dark" ? DarkTheme : DefaultTheme;
+
+  return (
+    <ThemeProvider value={theme}>
+      <InitialLayout />
+    </ThemeProvider>
+  );
+}
+
+export default RootLayout;
